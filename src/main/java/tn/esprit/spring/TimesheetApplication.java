4ea8169b5a70071462fce70af26e07c940bc09ec
amiribenabdellah.personal <amir.iben-abdellah@nexum.com>
2022-10-06 17:05:38 +0200

    jenkins

--- src/main/java/tn/esprit/spring/TimesheetApplication.java
+++ src/main/java/tn/esprit/spring/TimesheetApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableAutoConfiguration
 public class TimesheetApplication {
 
 	public static void main(String[] args) 
 	{SpringApplication.run(TimesheetApplication.class, args);}
-
  
 }
